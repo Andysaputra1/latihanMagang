02752ca0d511167a2d85be9eecc290c46faeee39
Andysaputra1 <andychensaputra@gmail.com>
2026-02-11 13:29:19 +0700

    update view companny

--- app/models/model_company.js
+++ app/models/model_company.js
@@ -3,7 +3,7 @@ const mysql = require('../modules/mysql_connector');
 const getCompany = async () => {
     try {
         const query = `SELECT * FROM ms_company ORDER BY company_name ASC`;
-        var rows = await mysql.execute(query, []);
+        var rows = await mysql.executeAsync(query, []);
         return [rows, null];
     } catch (error) {
         return [null, error];
@@ -14,7 +14,7 @@ const getCompany = async () => {
 const getCompanyById = async (id) => {
     try {
         const query = `SELECT * FROM ms_company WHERE company_id = ?`;
-        var rows = await mysql.execute(query, [id]);
+        var rows = await mysql.executeAsync(query, [id]);
         return [rows[0], null];
     } catch (error) {
         return [null, error];
@@ -25,7 +25,7 @@ const addCompany = async (data) => {
     try {
         const query = `INSERT INTO ms_company (company_name, company_phone, company_address) VALUES (?, ?, ?)`;
         const params = [data.name, data.phone, data.address];
-        var result = await mysql.execute(query, params);
+        var result = await mysql.executeAsync(query, params);
         return [result, null];
     } catch (error) {
         return [null, error];
@@ -35,7 +35,7 @@ const addCompany = async (data) => {
 const updateCompany = async (id, data) => {
     try {
         const query = `UPDATE ms_company SET company_name = ?, company_phone = ?, company_address = ? WHERE company_id = ?`;
-        var result = await mysql.execute(query, [data.name, data.phone, data.address, id]);
+        var result = await mysql.executeAsync(query, [data.name, data.phone, data.address, id]);
         return [result, null];
     } catch (error) {
         return [null, error];
@@ -46,7 +46,7 @@ const updateCompany = async (id, data) => {
 const deleteCompany = async (id) => {
     try {
         const query = `DELETE FROM ms_company WHERE company_id = ?`;
-        var result = await mysql.execute(query, [id]);
+        var result = await mysql.executeAsync(query, [id]);
         return [result, null];
     } catch (error) {
         return [null, error];
